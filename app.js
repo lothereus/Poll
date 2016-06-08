7cba88be64507f0f02de32765bd545e4074d2151
Lothereus <rolland.delhorbe@gmail.com>
2016-06-08 02:04:15 +0200

    First Poll commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// main
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+// middle
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(favicon(__dirname + '/public/lastwolfplay.ico'));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(multer());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle Errors gracefully
+app.use(function(err, req, res, next) {
+	if(!err) return next();
+	console.log(err.stack);
+	res.json({error: true});
+});
+
+// Main App Page
+app.get('/', routes.index);
+
+// MongoDB API Routes
+app.get('/polls/polls', routes.list);
+app.get('/polls/:id', routes.poll);
+app.post('/polls', routes.create);
+app.post('/vote', routes.vote);
+
+io.sockets.on('connection', routes.vote);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ models/Poll.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+// Subdocument schema for votes
+var voteSchema = new mongoose.Schema({ ip: 'String' });
+
+// Subdocument schema for poll choices
+var choiceSchema = new mongoose.Schema({ 
+	text: String,
+	votes: [voteSchema]
+});
+
+// Document schema for polls
+exports.PollSchema = new mongoose.Schema({
+	question: { type: String, required: true },
+	enddate: { type: Date, required: true },
+	choices: [choiceSchema]
+});
